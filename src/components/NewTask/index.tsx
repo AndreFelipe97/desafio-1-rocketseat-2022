aaaa5edac0bb6b2470417adc8b689a8df773647f
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2022-07-17 15:20:06 -0300

    feat: create NewTask

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,12 +1,33 @@
 import styles from "./NewTask.module.scss";
 import { FiPlusCircle } from "react-icons/fi";
+import { useState } from "react";
 
 export function NewTask() {
+  const [newTask, setNewTask] = useState<string>("");
+
+  function handleNewTaskChange() {
+    setNewTask(event.target.value);
+  }
+
+  const isNewTaskEmpity = newTask.length === 0;
+
   return (
     <div className={styles.container}>
       <form className={styles.formNewTask}>
-        <input className={styles.newTask} type="text" placeholder="Adicione uma nova tarefa" />
-        <button className={styles.addNewTask} type="submit">
+        <input
+          className={styles.newTask}
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          required
+          name="newTask"
+          onChange={handleNewTaskChange}
+          value={newTask}
+        />
+        <button
+          className={styles.addNewTask}
+          type="submit"
+          disabled={isNewTaskEmpity}
+        >
           Criar <FiPlusCircle />
         </button>
       </form>
