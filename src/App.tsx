19b68f1224c195db99df9d275431429efe5863d1
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2022-07-18 16:22:01 -0300

    feat: create task empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { FiClipboard } from "react-icons/fi";
+
 import { Header } from "./components/Header";
 import "./global.scss";
 import styles from "./App.module.scss";
@@ -9,7 +11,23 @@ function App() {
       <Header />
       <NewTask />
       <div className={styles.wrapper}>
-        <div>fkdngldngd</div>
+        <div className={styles.tasksInfo}>
+          <div className={styles.tasks}>
+            Tarefas criadas <span className={styles.countTasks}>0</span>
+          </div>
+          <div className={styles.tasksFinisehd}>
+            Concluídas <span className={styles.countTasks}>0</span>
+          </div>
+        </div>
+        <div className={styles.emptyTask}>
+          <FiClipboard />
+          <span className={styles.emptyTaskTitle}>
+            Você ainda não tem tarefas cadastras
+          </span>
+          <span className={styles.emptyTaskSubtitle}>
+            Crie tarefas e organize seus itens a fazer
+          </span>
+        </div>
       </div>
     </>
   );
